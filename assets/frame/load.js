0cf030b15525b8e84e9ffd81b73e1353479367c4
yourlocalfurryreal <maxwellwluo@gmail.com>
2024-12-05 10:49:45 -0500

    Update load.js

--- assets/frame/load.js
+++ assets/frame/load.js
@@ -2,7 +2,7 @@ const params = new URLSearchParams(window.location.search)
 if (params.get("game")) {
     games.forEach(async game => {
     if (game.id != params.get("game")) return
-    document.title = `${game.title} | DBP's Math`
+    document.title = `${game.title} | idk what I'm doing`
     document.querySelector("#gameImage").src = game.image
     document.querySelector("#gameTitle").innerHTML = game.title
     if (game.description) document.querySelector("#gameDescription").innerHTML = game.description
